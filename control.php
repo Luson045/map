e0c390dd933447b229b7580e451ad9365930dc22
Luson045 <lusonbasumatary2@gmail.com>
2024-04-28 12:34:21 +0530

    initial commit

---
+++ control.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Control</title>
+</head>
+<body>
+  <div>
+    <h1>CONTROL PANAL</h1>
+    <h2>Welcome<?php echo $_POST["hname"]?></h2>
+  </div>
+</body>
+</html>
